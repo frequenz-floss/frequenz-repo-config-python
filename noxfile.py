2be9e700bd2f23b0f2b7e4aee4bf5e9b4a909960
Leandro Lucarella <leandro.lucarella@frequenz.com>
2023-05-12 10:56:09 +0200

    Add test to check for unexpected changes in repo types
    Signed-off-by: Leandro Lucarella <leandro.lucarella@frequenz.com>

--- noxfile.py
+++ noxfile.py
@@ -5,8 +5,4 @@
 
 from frequenz.repo.config import nox
 
-# Remove the pytest sessions because we don't have tests yet
-config = nox.default.lib_config.copy()
-config.sessions = [s for s in config.sessions if not s.startswith("pytest")]
-
-nox.configure(config)
+nox.configure(nox.default.lib_config)
---
+++ tests/test_core.py
@@ -0,0 +1,20 @@
+# License: MIT
+# Copyright © 2023 Frequenz Energy-as-a-Service GmbH
+
+"""Tests for the core module."""
+
+from frequenz.repo.config import RepositoryType
+
+from . import utils
+
+
+def test_repository_type_doesnt_have_new_types() -> None:
+    """Test that `RepositoryType` doesn't have new types."""
+    expected = {
+        "actor",
+        "api",
+        "app",
+        "lib",
+    }
+    defined = {t.value for t in RepositoryType}
+    assert defined == expected, utils.MSG_UNEXPECTED_REPO_TYPES
