11521166edf6ac2a289cb540cea0c1e924c81bd7
Leandro Lucarella <luca-frequenz@llucax.com>
2023-06-30 09:42:17 +0200

    Adjust docs text to 88 columns
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- src/frequenz/repo/config/nox/__init__.py
+++ src/frequenz/repo/config/nox/__init__.py
@@ -3,8 +3,8 @@
 
 """Utilities to build noxfiles.
 
-The main entry point is the [`configure()`][frequenz.repo.config.nox.configure] function, which will
-configure all nox sessions according to some configuration.
+The main entry point is the [`configure()`][frequenz.repo.config.nox.configure]
+function, which will configure all nox sessions according to some configuration.
 
 To use the default options, you should call `configure()` using one of the [repository
 types][frequenz.repo.config.RepositoryType].  For example:
@@ -15,8 +15,9 @@ from frequenz.repo.config import RepositoryType, nox
 nox.configure(RepositoryType.LIB)
 ```
 
-Again, make sure to pick the correct project typedefault configuration based on the type of your
-project (`actor_config`, `api_config`, `app_config`, `lib_config`, `model_config`).
+Again, make sure to pick the correct project typedefault configuration based on the type
+of your project (`actor_config`, `api_config`, `app_config`, `lib_config`,
+`model_config`).
 
 If you need to use some custom configuration, you can start from the default settings in
 the [`frequenz.repo.config.nox.default`][] module,
@@ -32,16 +33,16 @@ config.opts.black.append("--diff")
 nox.configure(config)
 ```
 
-If you need further customization or to define new sessions, you can use the
-following modules:
+If you need further customization or to define new sessions, you can use the following
+modules:
 
-- [`frequenz.repo.config.nox.config`][]: Low-level utilities to configure nox sessions. It defines the
-  `Config` and CommandsOptions` classes and the actual implementation of the
-  `configure()` function. It also defines the `get()` function, which can be
-  used to get the currently used configuration object.
+- [`frequenz.repo.config.nox.config`][]: Low-level utilities to configure nox sessions.
+  It defines the `Config` and CommandsOptions` classes and the actual implementation of
+  the `configure()` function. It also defines the `get()` function, which can be used to
+  get the currently used configuration object.
 
-- [`frequenz.repo.config.nox.session`][]: Predefined nox sessions. These are the sessions that are used
-  by default.
+- [`frequenz.repo.config.nox.session`][]: Predefined nox sessions. These are the
+  sessions that are used by default.
 
 - [`frequenz.repo.config.nox.util`][]: General purpose utility functions.
 """
