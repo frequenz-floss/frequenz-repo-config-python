1e89c915973c95efdec8f1b1ced2c870dd9d5759
Leandro Lucarella <luca-frequenz@llucax.com>
2023-07-09 22:53:03 +0200

    Refactor integration tests
    This is mainly to make some parts more reusable for the upcoming golden
    tests.
    
    * The tests are now parametrized on the RepositoryType and not its
      value.
    * The repo generation was factored out to its own function.
    * The _run() function now has more options (check and capture_output,
      which are forwarded to subprocess.run())
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- tests/integration/test_cookiecutter_generation.py
+++ tests/integration/test_cookiecutter_generation.py
@@ -13,23 +13,11 @@ from frequenz.repo import config
 
 
 @pytest.mark.integration
-@pytest.mark.parametrize("repo_type", [t.value for t in config.RepositoryType])
-def test_generation(tmp_path: pathlib.Path, repo_type: str) -> None:
+@pytest.mark.parametrize("repo_type", [*config.RepositoryType])
+def test_generation(tmp_path: pathlib.Path, repo_type: config.RepositoryType) -> None:
     """Test generation of a new repo."""
     cwd = pathlib.Path().cwd()
-    _run(
-        tmp_path,
-        "cookiecutter",
-        "--no-input",
-        str(cwd / "cookiecutter"),
-        f"type={repo_type}",
-        "name=test",
-        "description=Test description",
-    )
-
-    subdirs = list(tmp_path.iterdir())
-    assert len(subdirs) == 1
-    repo_path = subdirs[0]
+    repo_path, _ = _generate_repo(repo_type, tmp_path)
     _run(repo_path, "python3", "-m", "venv", ".venv")
 
     _update_pyproject_repo_config_dep(
@@ -42,16 +30,47 @@ def test_generation(tmp_path: pathlib.Path, repo_type: str) -> None:
     subprocess.run(cmd, shell=True, cwd=repo_path, check=True)
 
 
-def _run(cwd: pathlib.Path, *cmd: str) -> subprocess.CompletedProcess[bytes]:
+def _generate_repo(
+    repo_type: config.RepositoryType,
+    tmp_path: pathlib.Path,
+    /,
+    *,
+    capture_output: bool = False,
+) -> tuple[pathlib.Path, subprocess.CompletedProcess[bytes]]:
+    cwd = pathlib.Path().cwd()
+    run_result = _run(
+        tmp_path,
+        "cookiecutter",
+        "--no-input",
+        str(cwd / "cookiecutter"),
+        f"type={repo_type.value}",
+        "name=test",
+        "description=Test description",
+        capture_output=capture_output,
+    )
+
+    subdirs = list(tmp_path.iterdir())
+    assert len(subdirs) == 1
+    repo_path = subdirs[0]
+    return repo_path, run_result
+
+
+def _run(
+    cwd: pathlib.Path, /, *cmd: str, capture_output: bool = False, check: bool = True
+) -> subprocess.CompletedProcess[bytes]:
+    """Run a command in a subprocess."""
     print()
     print("-" * 80)
     print(f"Running [{cwd}]: {' '.join(cmd)}")
     print()
-    return subprocess.run(cmd, cwd=cwd, check=True)
+    return subprocess.run(cmd, cwd=cwd, check=check, capture_output=capture_output)
 
 
 def _update_pyproject_repo_config_dep(
-    *, repo_config_path: pathlib.Path, repo_type: str, repo_path: pathlib.Path
+    *,
+    repo_config_path: pathlib.Path,
+    repo_type: config.RepositoryType,
+    repo_path: pathlib.Path,
 ) -> None:
     """Update the repo config dependency in the generated pyproject.toml.
 
@@ -64,8 +83,12 @@ def _update_pyproject_repo_config_dep(
         repo_type: Type of the repo to generate.
         repo_path: Path to the generated repo.
     """
-    repo_config_dep = f"frequenz-repo-config[{repo_type}] @ file://{repo_config_path}"
-    repo_config_dep_re = re.compile(rf"""frequenz-repo-config\[{repo_type}\][^"]+""")
+    repo_config_dep = (
+        f"frequenz-repo-config[{repo_type.value}] @ file://{repo_config_path}"
+    )
+    repo_config_dep_re = re.compile(
+        rf"""frequenz-repo-config\[{repo_type.value}\][^"]+"""
+    )
 
     with open(repo_path / "pyproject.toml", encoding="utf8") as pyproject_file:
         pyproject_content = pyproject_file.read()
